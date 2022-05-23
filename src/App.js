7582367721d68216190fb0ed35c47423750fe78c
isshin123 <isshin95@gmail.com>
2022-05-23 14:22:25 +0900

    basic Header Component design

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Orders from "./components/Orders";
+import Prototypes from "./components/Prototypes";
+
 function App() {
   return (
     <>
