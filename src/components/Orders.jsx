733fb04f47d39cf7b7a695682fe145446cd1b24c
isshin123 <isshin95@gmail.com>
2022-05-24 14:28:39 +0900

    order remove, add interactions visual ui update

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,7 +1,23 @@
+import { useMemo } from "react";
+import useActions from "../hooks/useActions";
 import useOrders from "../hooks/useOrders";
+import usePrototypes from "../hooks/usePrototypes";
 
 export default function Orders() {
   const orders = useOrders();
+  const prototypes = usePrototypes();
+  const { remove, removeAll } = useActions();
+
+  const totalPrice = useMemo(() => {
+    return orders
+      .map((order) => {
+        const { id, quantity } = order;
+        const prototpye = prototypes.find((p) => p.id === id);
+        return prototpye.price * quantity;
+      })
+      .reduce((l, r) => l + r, 0);
+  }, [orders, prototypes]);
+
   console.log(orders);
 
   if (orders.length === 0) {
@@ -15,5 +31,56 @@ export default function Orders() {
     );
   }
 
-  return <aside>orders</aside>;
+  return (
+    <aside>
+      <div className="order">
+        <div className="body">
+          {orders.map((order) => {
+            const { id } = order;
+            const prototype = prototypes.find((p) => p.id === id);
+            const click = () => {
+              remove(id);
+            };
+
+            return (
+              <div className="item" key={id}>
+                <div className="img">
+                  <video src={prototype.thumbnail} />
+                </div>
+                <div className="content">
+                  <p className="title">
+                    {prototype.title} x {order.quantity}
+                  </p>
+                </div>
+                <div className="action">
+                  <p className="price">$ {prototype.price * order.quantity}</p>
+                  <button className="btn btn--link" onClick={click}>
+                    <i className="icon icon--cross" />
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="total">
+          <hr />
+          <div className="item">
+            <div className="content">Total</div>
+            <div className="action">
+              <div className="price">$ {totalPrice}</div>
+            </div>
+            <button className="btn btn--link">
+              <i className="icon icon--delete" onClick={removeAll} />
+            </button>
+          </div>
+          <button
+            className="btn btn--secondary"
+            style={{ width: "100%", marginTop: 10 }}
+          >
+            Checkout
+          </button>
+        </div>
+      </div>
+    </aside>
+  );
 }
